7f04e3a4cd92ff470a96f972fd1d342bf90f7365
Ruimin Li <rl751@scarletmail.rutgers.edu>
2019-10-26 18:58:33 +0000

    Knight.java edited online with Bitbucket Knight basic

--- Chess12/src/structure/Knight.java
+++ Chess12/src/structure/Knight.java
@@ -1,5 +1,7 @@
 package structure;
 
+import chess.Chess;
+
 public class Knight extends Cell{
 
 	
@@ -8,10 +10,34 @@ public class Knight extends Cell{
 		super(cellName, pieceName, isAlive, x, y);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public boolean Ownside(String name1,String name2)
+	{
+		switch (name1) {
+		case "wN": if (name2 == "wR" || name2== "wN" || name2== "wB" || name2== "wQ" || name2 == "wK" || name2 == "wp")
+						return true;
+		            else
+		            	return false;
+		default : if (name2 == "bR" || name2== "bN" || name2== "bB" || name2== "bQ" || name2 == "bK" || name2 == "bp")
+						return true;
+					else
+						return false;
+		}
+	}
+	public boolean JudgePos(int currX, int currY, int tarX, int tarY)
+	{
+		if((tarX==currX+1 && tarY==currY-2) || (tarX==currX+2 && tarY==currY-1) || (tarX==currX+2 && tarY==currY+1) || (tarX==currX+1 && tarY==currY+2) || (tarX==currX-1 && tarY==currY+2) || (tarX==currX-2 && tarY==currY+1) || (tarX==currX-2 && tarY==currY-1) || (tarX==currX-1 && tarY==currY-2))
+			return true;
+		else
+			return false;
+	}
 	@Override
 	public boolean move(int currX, int currY, int tarX, int tarY) {
-		// TODO Auto-generated method stub
+		if(tarX>=0 && tarX<=7 && tarY>=0 && tarY<=7 && JudgePos(currX,currY,tarX,tarY) && !(Ownside(Chess.board[currX][currY].pieceName,Chess.board[tarX][tarY].pieceName)))
+		{
+			jump(currX,currY,tarX,tarY);
+			return true;
+		}	
 		return false;
 	}
 
