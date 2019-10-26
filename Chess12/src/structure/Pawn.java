134bf2adf870288388fdad8c00e83ca02816978e
Ruimin Li <rl751@scarletmail.rutgers.edu>
2019-10-26 05:17:11 +0000

    Pawn.java edited online with Bitbucket  pawn basic

--- Chess12/src/structure/Pawn.java
+++ Chess12/src/structure/Pawn.java
@@ -1,18 +1,129 @@
 package structure;
 
-public class Pawn extends Cell{
+import java.util.*;
 
-	
+import chess.Chess;
 
-	public Pawn(String cellName, String pieceName, boolean isAlive, int x, int y) {
+public class Pawn extends Cell {
+ 
+	public boolean Ownside(String name1,String name2)
+	{
+		switch (name1) {
+		case "wp": if (name2 == "wR" || name2== "wN" || name2== "wB" || name2== "wQ" || name2 == "wK" || name2 == "wp")
+						return true;
+		            else
+		            	return false;
+		default : if (name2 == "bR" || name2== "bN" || name2== "bB" || name2== "bQ" || name2 == "bK" || name2 == "bp")
+						return true;
+					else
+						return false;
+		}
+	}
+    public Pawn(String cellName, String pieceName, boolean isAlive, int x, int y) {
 		super(cellName, pieceName, isAlive, x, y);
-		// TODO Auto-generated constructor stub
+		
 	}
 
-	@Override
 	public boolean move(int currX, int currY, int tarX, int tarY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+		if(currX==1 && pieceName.equals("wp"))
+		{
+			if(tarX==currX+1 && tarY==currY && Chess.board[currX+1][currY].pieceName.equals("empty"))
+			{
+				jump(currX,currY,tarX,tarY);
+				return true;
+			}
+			else if(tarX==currX+1 && tarY==currY && !(Chess.board[currX+1][currY].pieceName.equals("empty")))
+				return false;
+			else if(tarX==currX+2 && tarY==currY && Chess.board[currX+2][currY].pieceName.equals("empty"))
+			{
+				jump(currX,currY,tarX,tarY);
+				return true;
+			}
+			else if(tarX==currX+2 && tarY==currY && !(Chess.board[currX+2][currY].pieceName.equals("empty")))
+				return false;
+			else if(tarX==currX+1 && tarY == (currY-1) && !(Ownside(pieceName, Chess.board[currX+1][currY-1].pieceName)) && tarY>=0)
+			{
+				jump(currX,currY,tarX,tarY);
+				return true;
+			}
+			else if(tarX==currX+1 && tarY == (currY+1) && !(Ownside(pieceName, Chess.board[currX+1][currY+1].pieceName)) && tarY<=7)
+			{
+				jump(currX,currY,tarX,tarY);
+				return true;
+			}
+			
+		}
+		else if(currX==6 && pieceName.equals("bp"))
+		{
+			if(tarX==(currX-1) && tarY==currY && Chess.board[currX-1][currY].pieceName.equals("empty"))
+			{
+				jump(currX,currY,tarX,tarY);
+				return true;
+			}
+			else if(tarX==(currX-1) && tarY==currY && !(Chess.board[currX-1][currY].pieceName.equals("empty")))
+				return false;
+			else if(tarX==(currX-2) && tarY==currY && Chess.board[currX-2][currY].pieceName.equals("empty"))
+			{
+				jump(currX,currY,tarX,tarY);
+				return true;
+			}
+			else if(tarX==currX-2 && tarY==currY && !(Chess.board[currX-2][currY].pieceName.equals("empty")))
+				return false;
+			else if(tarX==currX-1 && tarY == (currY-1) && !(Ownside(pieceName, Chess.board[currX-1][currY-1].pieceName)) && tarY>=0)
+			{
+				jump(currX,currY,tarX,tarY);
+				return true;
+			}
+			else if(tarX==currX-1 && tarY == (currY+1) && !(Ownside(pieceName, Chess.board[currX-1][currY+1].pieceName)) && tarY<=7)
+			{
+				jump(currX,currY,tarX,tarY);
+				return true;
+			}
+		}
+		else if(currX!=0 && currX!=7)
+		{
+			if(pieceName.equals("wp"))
+			{
+				if(tarX==currX+1 && tarY==currY && Chess.board[currX+1][currY].pieceName.equals("empty"))
+				{
+					jump(currX,currY,tarX,tarY);
+					return true;
+				}
+				else if(tarX==currX+1 && tarY==currX && !(Chess.board[currX+1][currY].pieceName.equals("empty")))
+					return false;
+				else if(tarX==currX+1 && tarY == (currY-1) && !(Ownside(pieceName, Chess.board[currX+1][currY-1].pieceName)) && tarY>=0)
+				{
+					jump(currX,currY,tarX,tarY);
+					return true;
+				}
+				else if(tarX==currX+1 && tarY == (currY+1) && !(Ownside(pieceName, Chess.board[currX+1][currY+1].pieceName)) && tarY<=0)
+				{
+					jump(currX,currY,tarX,tarY);
+					return true;
+				}
+			}
+			if(pieceName.equals("bp"))
+			{
+				if(tarX==(currX-1) && tarY==currY && Chess.board[currX-1][currY].pieceName.equals("empty"))
+				{
+					jump(currX,currY,tarX,tarY);
+					return true;
+				}
+				else if(tarX==(currX-1) && tarY==currY && !(Chess.board[currX-1][currY].pieceName.equals("empty")))
+					return false;
+				else if(tarX==currX-1 && tarY == (currY-1) && !(Ownside(pieceName, Chess.board[currX-1][currY-1].pieceName)) && tarY>=0)
+				{
+					jump(currX,currY,tarX,tarY);
+					return true;
+				}
+				else if(tarX==currX-1 && tarY == (currY+1) && !(Ownside(pieceName, Chess.board[currX-1][currY+1].pieceName)) && tarY<=7)
+				{
+					jump(currX,currY,tarX,tarY);
+					return true;
+				}
+			}
+		}
+			return false;
+    }
 }
+
