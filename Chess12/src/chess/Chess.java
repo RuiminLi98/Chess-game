be3e8b1267e412fd8af71ebbf8fee353a2f4441f
Ruimin Li <rl751@scarletmail.rutgers.edu>
2019-10-27 21:34:47 -0400

    resture bug for draw and resign

--- Chess12/src/chess/Chess.java
+++ Chess12/src/chess/Chess.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 import structure.*;
 
 public class Chess {
-	
+
 	public static Cell[][] board;
-	
+
 	public static boolean CastlingWL;
 	public static boolean CastlingBL;
 	public static boolean CastlingBS;
 	public static boolean CastlingWS;
-	
+
 	public static boolean WCheck;
 	public static boolean BCheck;
 	public static boolean draw_flag;
-	
+
 	public static void Initialize(){
 		CastlingBL=true;
 		CastlingWL=true;
@@ -36,8 +36,8 @@ public class Chess {
 //		board[0][6]=new Empty("##", "empty", false, 0, 6);
 		//
 		board[0][7]=new Rook("  ", "wR", true, 0, 7);
-		
-		
+
+
 		board[7][0]=new Rook("  ", "bR", true, 7, 0);
 		board[7][1]=new Knight("##", "bN", true,7, 1);
 		board[7][2]=new Bishop("  ", "bB", true,7, 2);
@@ -46,7 +46,7 @@ public class Chess {
 		board[7][5]=new Bishop("##", "bB", true,7, 5);
 		board[7][6]=new Knight("  ", "bN", true,7, 6);
 		board[7][7]=new Rook("##", "bR", true,7, 7);
-		
+
 		for(int i=0;i<8;i+=2) {
 			board[1][i]=new Pawn("  ", "wp", true, 1, i);
 			board[6][i]=new Pawn("##", "bp", true, 6, i);
@@ -55,7 +55,7 @@ public class Chess {
 			board[1][i]=new Pawn("##", "wp", true, 1, i);
 			board[6][i]=new Pawn("  ", "bp", true, 6, i);
 		}
-		
+
 		for(int i=0;i<8;i+=2) {
 			//
 //			board[1][i]=new Empty("  ", "empty", false, 1, i);
@@ -77,7 +77,7 @@ public class Chess {
 			board[5][i]=new Empty("##", "empty", false, 5, i);
 		}
 	}
-	
+
 	public static void printBoard() {
 		for(int i=7;i>=0;i--) {
 			for(int j=0;j<8;j++) {
@@ -105,13 +105,13 @@ public class Chess {
 		if(Character.isDigit(c))return Integer.parseInt(c+"")-1;
 		return (int)c-97;
 	}
-	
+
 	public static int isCheckmate() {
 		//return 0 for not checkmate
 		//return 1 for white king is checked
 		//return 2 for black king is checked
 		//return 3 for both
-		
+
 		Point WKLoc=Point.getLocation("wK");
 		Point BKLoc=Point.getLocation("bK");
 		if(Point.check(WKLoc)&&Point.check(BKLoc))return 3;
@@ -119,7 +119,7 @@ public class Chess {
 		if(Point.check(BKLoc))return 2;
 		return 0;
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Initialize();
@@ -131,9 +131,9 @@ public class Chess {
 			String end="";
 			char turn=' ';
 			String line="";
-			if(count%2==0) 
+			if(count%2==0)
 				System.out.print("White's move: ");
-				
+
 			else
 				System.out.print("Black's move: ");
 			line=sc.nextLine();
@@ -141,45 +141,45 @@ public class Chess {
 			int num=a.length;
 			start=a[0];
 			if(start.equals("draw") && draw_flag==true)
-			 {
-				
+			{
+
 				System.out.println("We draw successfully");
-				 return;
-			 }
-			else 
+				return;
+			}
+			else
 			{
 				draw_flag=false;
 			}
 			if(count%2==0) {
-				 if(start.equals("resign"))
-				 {
-					 System.out.println("Black wins");
-					 return;
-				 }	 
-				 else
-				 {
-				 end=a[1];
-				 	if(num==3) {
-				 		if(a[2].equals("draw?"))
-				 			draw_flag=true;}
-				 turn='w';
-				 System.out.println();}
+				if(start.equals("resign"))
+				{
+					System.out.println("Black wins");
+					return;
+				}
+				else
+				{
+					end=a[1];
+					if(num==3) {
+						if(a[2].equals("draw?"))
+							draw_flag=true;}
+					turn='w';
+					System.out.println();}
 			}else {
-				
-				 if(start.equals("resign"))
-				 {
-					 System.out.println("White wins");
-					 return;
-				 }
-				 else
-				 {
-				 end=a[1];
-				 	if(num==3) {
-				 		if(a[2].equals("draw?"))
-				 			draw_flag=true;}
-				 turn='b';
-				System.out.println();
-				 		}
+
+				if(start.equals("resign"))
+				{
+					System.out.println("White wins");
+					return;
+				}
+				else
+				{
+					end=a[1];
+					if(num==3) {
+						if(a[2].equals("draw?"))
+							draw_flag=true;}
+					turn='b';
+					System.out.println();
+				}
 			}
 			count++;
 			int curX=parseLocation(start.charAt(1));
@@ -188,15 +188,68 @@ public class Chess {
 			int tarY=parseLocation(end.charAt(0));
 			while((Chess.board[curX][curY].pieceName.charAt(0)!=turn)||!Chess.board[curX][curY].move(curX, curY, tarX, tarY)) {
 				System.out.print("Illegal move, try again: ");
-				start=sc.next();
-				end=sc.next();
-				System.out.println();
+				if(count%2==1) {  turn='w';
+					line=sc.nextLine();
+					a=line.split(" ");
+					num=a.length;
+					start=a[0];
+					if(start.equals("draw") && draw_flag==true)
+					{
+
+						System.out.println("We draw successfully");
+						return;
+					}
+					else
+					{
+						draw_flag=false;
+					}
+					if(start.equals("resign"))
+					{
+						System.out.println("Black wins");
+						return;
+					}
+					else
+					{
+						end=a[1];
+						if(num==3) {
+							if(a[2].equals("draw?"))
+								draw_flag=true;}
+						System.out.println();}}
+				else
+				{turn='b';
+					line=sc.nextLine();
+					a=line.split(" ");
+					num=a.length;
+					start=a[0];
+					if(start.equals("draw") && draw_flag==true)
+					{
+
+						System.out.println("We draw successfully");
+						return;
+					}
+					else
+					{
+						draw_flag=false;
+					}
+					if(start.equals("resign"))
+					{
+						System.out.println("Black wins");
+						return;
+					}
+					else
+					{
+						end=a[1];
+						if(num==3) {
+							if(a[2].equals("draw?"))
+								draw_flag=true;}
+						System.out.println();}
+				}
 				curX=parseLocation(start.charAt(1));
 				curY=parseLocation(start.charAt(0));
 				tarX=parseLocation(end.charAt(1));
 				tarY=parseLocation(end.charAt(0));
 			}
-				printBoard();
+			printBoard();
 
 		}
 	}
